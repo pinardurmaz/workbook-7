4cfd30e60d539b114613a39fd724df5149af9606
Pinar Durmaz <pnr.drmz02@gmail.com>
2025-06-04 15:20:28 -0400

    Complated Nested Queries

---
+++ aggregates-and-grouping/exercises.sql
@@ -0,0 +1,45 @@
+USE northwind;
+-- 1. How many suppliers are there? Use a query!
+SELECT COUNT(*) AS supplier_count
+FROM suppliers;
+
+-- 2. What is the sum of all the employee's salaries?
+SELECT SUM(Salary) AS total_salary
+FROM employees;
+
+-- 3. What is the price of the cheapest item that Northwind sells?
+SELECT MIN(UnitPrice) AS cheapest_item
+FROM products;
+
+-- 4. What is the average price of items that Northwind sells?
+SELECT AVG(UnitPrice) AS average_price
+FROM products;
+
+-- 5. What is the price of the most expensive item that Northwind sells?
+SELECT MAX(UnitPrice) AS most_expensive
+FROM products;
+
+-- 6. What is the supplier ID of each supplier and the number of items they supply?
+-- You can answer this query by only looking at the Products table.
+SELECT SupplierID, COUNT(*) AS item_count
+FROM products
+GROUP BY SupplierID;
+
+-- 7. What is the category ID of each category and the average price of each item in the category? 
+-- You can answer this query by only looking at the Products table.
+SELECT CategoryID, AVG(UnitPrice) AS avg_price
+FROM products
+GROUP BY CategoryID;
+
+-- 8. For suppliers that provide at least 5 items to Northwind, what is the supplier ID of each supplier and
+ -- the number of items they supply? You can answer this query by only looking at the Products table.
+SELECT SupplierID, COUNT(*) AS item_count
+FROM products
+GROUP BY SupplierID
+HAVING COUNT(*) >= 5;
+
+-- 9. List the product id, product name, and inventory value (calculated by multiplying unit price by the number of units on hand).
+-- Sort the results in descending order by value. If two or more have the same value, order by product name.
+SELECT ProductID, ProductName, (UnitPrice * UnitsInStock) AS inventory_value
+FROM Products
+ORDER BY inventory_value DESC, ProductName ASC;
